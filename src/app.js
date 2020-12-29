28cc4e12d9597f16c2147d6dbac73b822576b9d2
Jordan.Yarros.15@cnu.edu <jordan.yarros.15@cnu.edu>
2020-12-29 10:52:05 -0500

    Add private routes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {startSetExpenses} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
@@ -22,16 +22,30 @@ const jsx = (
   <AppRouter/>
 </Provider>
 );
-ReactDom.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDom.render(jsx, document.getElementById('app'));
-});
+let hasRendered = false;
+const renderApp = () => {
+  if(!hasRendered){
+    ReactDom.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
+ReactDom.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user){
-    console.log('in');
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if(history.location.pathname === '/'){
+        history.push('/dashboard');
+      }
+    });
+
   } else {
+    store.dispatch(logout());
+    renderApp();
     history.push('/');
   }
 });
