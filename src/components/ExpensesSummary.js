36e5d964e7ae797a52b68f7cf3c43768281e8e8e
Jordan.Yarros.15@cnu.edu <jordan.yarros.15@cnu.edu>
2020-12-30 15:30:02 -0500

    fix spelling

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -15,7 +15,7 @@ export const ExpensesSummary = ({expenseCount , expensesTotal }) => {
             <div className="page-header">
                 <div className="content-container">
                     <h1 className = "page-header__title">
-                        Viewing <span> {expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal} </span>
+                        Viewing <span> {expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal} </span>
                     </h1>
                     <div className="page-header__actions">
                         <Link to="/create" className="button">Create Expense</Link> 
